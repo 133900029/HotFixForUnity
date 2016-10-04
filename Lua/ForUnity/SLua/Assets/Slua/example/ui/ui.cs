7ee2c9c0905ed7b8f4db5b8ae98a9660405616f1
Martin <133900029@qq.com>
2016-10-05 03:08:06 +0800

    Button&Image&Text&EventTrigger

--- Lua/ForUnity/SLua/Assets/Slua/example/ui/ui.cs
+++ Lua/ForUnity/SLua/Assets/Slua/example/ui/ui.cs
@@ -2,7 +2,9 @@
 using System.Collections;
 using UnityEngine.UI;
 using LuaInterface;
+using UnityEngine.EventSystems;
 using SLua;
+using UnityEngine.Events;
 public class ui : MonoBehaviour {
 
 	// Use this for initialization
@@ -14,7 +16,22 @@ public class ui : MonoBehaviour {
         {
             luaSvr.start("uiLua");
         }, LuaSvrFlag.LSF_DEBUG);
- 
+
+        GameObject button = GameObject.Find("Button");
+        button.GetComponent<Button>().onClick.AddListener(() => {
+            Debug.Log("Click");
+       });
+
+        GameObject image = GameObject.Find("Image");
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.callback.AddListener((BaseEventData data) =>
+        {
+            Debug.Log("Click Image");
+        });
+        
+        image.AddComponent<EventTrigger>().triggers.Add(entry);
+
+
 	}
 	
 	// Update is called once per frame
@@ -22,6 +39,10 @@ public class ui : MonoBehaviour {
 	
 	}
 }
+
+//需要看下屏幕适配
+
+//重启unity能解决按钮点击不了的问题
 
 //canvas
 //screen space - overlay
@@ -79,3 +100,42 @@ public class ui : MonoBehaviour {
 
 //anchors 不一样的时候可以实现根据父对象进行缩放
 //anchors 一样的时候可以实现在父对象的指定位置
+
+
+
+
+//button
+//interactable 是否交互
+
+//colortine
+//transition为过渡方式
+//color multiple对过度的颜色进行叠加
+//fade duration 过度时间
+
+//sprite swap
+//用图片切换
+
+//animation
+//用状态机进行切换
+
+//navigation 为用键盘控制
+
+
+//ui image
+//当没有source image 的时候，ui控件只会显示颜色，没有轮廓
+//image type:
+//simple:完全填充 preserve aspect：按百分比拉伸，超出就不拉伸 和set native size 差不多
+//sliced:方格外部不拉伸，只拉伸中间 fill center:九宫格中间是否显示
+//tiled:重复显示超出的部分
+//filled:根据不同的填充方式从左到右填或者按圆心填充
+
+//UI text
+//Text：显示文字
+//line space：行距
+//horizontal overflow：wrap ：超出会看不见，overflow：允许超出
+//vertical overflow：truncate：超出会看不见，overflow：允许超出
+//best fit：忽略overflow选项，通过修改字体大小，使所有字体显示
+
+//UI Events and Event triggers
+//给ui image 直接添加event trigger 可以实现点击功能
+//event system 决定如何相应其他ui（通过standalone模式或者touch模式）
