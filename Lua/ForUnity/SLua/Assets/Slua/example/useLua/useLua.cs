dac81c77a932c32b5cccec65525def27003e377e
Martin <133900029@qq.com>
2016-10-06 22:05:21 +0800

    Use Lua to build ui

---
+++ Lua/ForUnity/SLua/Assets/Slua/example/useLua/useLua.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using SLua;
+using LuaInterface;
+
+
+public class useLua : MonoBehaviour {
+
+	// Use this for initialization
+
+
+    private LuaSvr luaSvr;
+	void Start () {
+        luaSvr = new LuaSvr();
+        luaSvr.init(null, () => {
+            luaSvr.start("useLua");
+        }, LuaSvrFlag.LSF_DEBUG);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
